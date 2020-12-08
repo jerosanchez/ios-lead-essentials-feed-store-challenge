e74582cc8b0ef5f801b6d4ba37028dfe88ef38d7
Jero Sanchez <jero.sanchez@icloud.com>
2020-12-08 17:33:51 +0100

    Delete delivers no error on empty cache

--- FeedStoreChallenge/UserDefaultsFeedStore.swift
+++ FeedStoreChallenge/UserDefaultsFeedStore.swift
@@ -47,6 +47,7 @@ public class UserDefaultsFeedStore: FeedStore {
 	}
 	
 	public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+		completion(nil)
 	}
 	
 	public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
